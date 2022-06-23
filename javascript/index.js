6ebd085ad818dfab3acf58804ec57ef8c7765580
Blesyn1 <105324616+Blesyn1@users.noreply.github.com>
2022-06-23 13:43:34 +0100

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+// Introductory page!
+let first_name = "Blessing"
+let last_name = "Essien"
+console.log("I'm " + first_name + " " +last_name);
+let height = "Height: 5.3ft";
+console.log(height);
+let country = "Country: Nigeria";
+console.log(country);
+
